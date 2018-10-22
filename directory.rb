a97175fa1c17f6c828c000ee9b94b8257d5c4382
Aaron Gill <izzydiggler@gmail.com>
2018-10-22 18:30:27 +0100

    list of students removed and method added to get user input for students

--- directory.rb
+++ directory.rb
@@ -1,20 +1,25 @@
 # array of students
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+
+def input_students
+
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+
+    students << {name: name, cohort: "november"}
+    puts "We now have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
-puts  "The students of Villains Academy"
+puts  "The students of my cohort at Makers Academy"
 puts "-------------"
 end
 
@@ -27,6 +32,7 @@ def print_footer(names)
  puts "Overall, we have #{names.count} great students"
 end
 end
+students = input_students
 print_header
 print_students(students)
 print_footer(students)
