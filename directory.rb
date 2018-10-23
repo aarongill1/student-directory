3663f939a9e7476ec4458d94fbc6da181a64b121
Aaron Gill <izzydiggler@gmail.com>
2018-10-23 16:55:52 +0100

    refactored to work on command line and added method to allow load of existing students on startup

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def interactive_menu
 
   loop do
   print_menu
-  process(gets.chomp)
+  process(STDIN.gets.chomp)
   end
 end
 
@@ -38,12 +38,12 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  name = gets.chomp
+  name = STDIN.gets.chomp
 
   while !name.empty? do
     @students << {name: name, cohort: "november"}
     puts "We now have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -59,7 +59,7 @@ end
   end
 
   def print_students_list
-  @students.each_with_index do |student, index|
+  @students.each.with_index(1) do |student, index|
 
     puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -78,12 +78,27 @@ end
     end
     file.close
   end
-  def load_students
-    file = File.open("students.csv", "r")
+
+  def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
     file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
   end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} does not exist"
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
