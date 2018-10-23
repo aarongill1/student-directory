6548e62b33972604f4ebbf79e6e4b8e1d0b80cd6
Aaron Gill <izzydiggler@gmail.com>
2018-10-23 11:54:12 +0100

    more refactoring and additional methods added to break down larger methods into more readable code

--- directory.rb
+++ directory.rb
@@ -1,60 +1,67 @@
+@students = []
+
+def interactive_menu
+
+  loop do
+  print_menu
+  process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else puts "Invalid selection - try again"
+  end
+  end
+
 def input_students
 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
-
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: "november"}
-    puts "We now have #{students.count} students"
+    @students << {name: name, cohort: "november"}
+    puts "We now have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
-def print_header
-puts  "The students of my cohort at Makers Academy"
-puts "-------------"
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
-def print_students(students)
-students.each_with_index do |student, index|
+  def print_header
+  puts  "The students of my cohort at Makers Academy"
+  puts "-------------"
+  end
 
-  puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
-end
+  def print_students_list
+  @students.each_with_index do |student, index|
 
-def print_footer(names)
- puts "Overall, we have #{names.count} great students"
-end
-end
-
-def interactive_menu
-
-  students = []
-
-  loop do
-
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-
-  selection = gets.chomp
-
-  case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_students(students)
-      print_footer(students)
-    when "9"
-      exit
-    else "Invalid selection - try again"
+    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
   end
   end
-end
+
+  def print_footer
+   puts "Overall, we have #{@students.count} great students"
+
+  end
 
 interactive_menu
