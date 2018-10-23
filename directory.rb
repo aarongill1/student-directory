555acdcb698ba32e1c4465ce96f7a0f41a7ac0fa
Aaron Gill <izzydiggler@gmail.com>
2018-10-23 14:57:59 +0100

    added method to save output to file

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -21,6 +22,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else puts "Invalid selection - try again"
@@ -61,7 +64,15 @@ end
 
   def print_footer
    puts "Overall, we have #{@students.count} great students"
-
   end
 
+  def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+    file.close
+  end
 interactive_menu
