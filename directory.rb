31f1bc93760a0860fdbe92069c3844c00cb60738
Aaron Gill <izzydiggler@gmail.com>
2018-10-23 10:34:48 +0100

    interactive menu added - not yet complete but existing methods added to it

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-# array of students
-
 def input_students
 
   puts "Please enter the names of the students"
@@ -10,7 +8,6 @@ def input_students
   name = gets.chomp
 
   while !name.empty? do
-
     students << {name: name, cohort: "november"}
     puts "We now have #{students.count} students"
     name = gets.chomp
@@ -24,15 +21,40 @@ puts "-------------"
 end
 
 def print_students(students)
-students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+students.each_with_index do |student, index|
+
+  puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
 end
 
 def print_footer(names)
  puts "Overall, we have #{names.count} great students"
 end
 end
-students = input_students
-print_header
-print_students(students)
-print_footer(students)
+
+def interactive_menu
+
+  students = []
+
+  loop do
+
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+
+  selection = gets.chomp
+
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print_students(students)
+      print_footer(students)
+    when "9"
+      exit
+    else "Invalid selection - try again"
+  end
+  end
+end
+
+interactive_menu
